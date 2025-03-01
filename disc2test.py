ea88bd3953a23e1b26139bc7820c02178b11659a
Bob Denny <rdenny@dc3.com>
2025-03-01 16:51:27 -0700

    Old (discovery) and new (discovery2) and new test (disc2test) from Linux. IPV4 works fine in discovery2.

---
+++ disc2test.py
@@ -0,0 +1,11 @@
+from alpaca import discovery2, management
+
+svrs = discovery2.search_ipv4(1,1)
+print(svrs)
+for svr in svrs:
+   print(f"At {svr}")
+   print (f"  V{management.apiversions(svr)} server")
+   print (f"  {management.description(svr)['ServerName']}")
+   devs = management.configureddevices(svr)
+   for dev in devs:
+       print(f"    {dev['DeviceType']}[{dev['DeviceNumber']}]: {dev['DeviceName']}")
