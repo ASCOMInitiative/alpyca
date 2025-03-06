405be2bfb8e4579be7ae4a3fc93301d1f28a46ec
Bob Denny <rdenny@dc3.com>
2025-03-06 16:24:16 -0700

    Tests OK on V4/V6, Windows/Linux. Ready for field test.

--- alpaca/discovery.py
+++ alpaca/discovery.py
@@ -13,7 +13,7 @@
 # About netifaces 0.11.0 package: This is now abandonware, Issue #17 requests
 # a change. I looked at netifaces2 but it is incompatible (long story) but
 # netifaces-plus appears to be a fork of the original al45tair/netifaces and
-# it appears to work. This 0.3.1 edit has been tested with netifaces-plus.
+# it appears to work.
 #
 # -----------------------------------------------------------------------------
 # MIT License
@@ -44,21 +44,23 @@
 #                 default timeout is 2 sec.
 # 13-May-22 (rbd) 2.0.0-dev1 Project now called "Alpyca" - no logic changes
 # 21-Aug-22 (rbd) 2.0.2 Fix multicast to 127.0.0.1 GitHub Issue #6
-# 01-Mar-25 (rbd) 3.1.0 Save this with improvements resulting from tests aimed
-#                       at eventually moving to use of ifaddr instead of
-#                       netifaces. See improved comments.
+# 06-Mar-25 (rbd) 3.1.0 Fix Issue #17 by ditching netifaces and friends, and
+#                       using ifaddr while detecting inactive IPs another way.
 # -----------------------------------------------------------------------------
 
-import json
 import time
+import json
 import socket
-import netifaces
 import platform
 from typing import List
+import ipaddress
+import ifaddr
+# https://ifaddr.readthedocs.io/latest/
 
 port = 32227
 AlpacaDiscovery = "alpacadiscovery1"
 AlpacaResponse = "AlpacaPort"
+
 
 def search_ipv4(numquery: int=2, timeout: int=2) -> List[str]:
     """Discover Alpaca device servers on the IPV4 LAN/VLAN
@@ -78,7 +80,10 @@ def search_ipv4(numquery: int=2, timeout: int=2) -> List[str]:
 
     Note:
         * This function uses IPV4
-        * UDP protocol using multicasts and restricted to the LAN/VLAN is used to perform the query.
+        * UDP protocol restricted to the LAN/VLAN is used to perform the query.
+        * Adapters which show an APIPA address (169.254.*.*) are skipped in order to avoid a
+          guaranteed timeout trying to receive the datagram replies. See
+          `Why Do I Have the 169.254 IP Address? <https://www.whatismyip.com/169-254-ip-address/>`_
         * See section 4 of the `Alpaca API Reference <https://ascom-standards.org/AlpacaDeveloper/ASCOMAlpacaAPIReference.html>`_
           for Discovery details.
 
@@ -89,48 +94,43 @@ def search_ipv4(numquery: int=2, timeout: int=2) -> List[str]:
     try:
         sock.bind(('0.0.0.0', 0))  # listen to any on a temporary port
     except:
-        print('failure to bind')
+        # print('failure to bind')
         sock.close()
         raise
 
     addrs = []
     try:
         for i in range(numquery):
-            for interface in netifaces.interfaces():
-                for interfacedata in netifaces.ifaddresses(interface):  # Sift through interfaces
-                    if netifaces.AF_INET == interfacedata:             # Consider only those with IPv4
-                        for ip in netifaces.ifaddresses(interface)[netifaces.AF_INET]:  # Each IPv4 address on this interface
-                            addr = ip['addr']
-                            print(ip)
-                            if('broadcast' in ip):                  # Windows this includes localhost
-                                sock.sendto(AlpacaDiscovery.encode(),
-                                            (ip['broadcast'], port))
-                            elif('peer' in ip):                     # Linux localhost tagged 'peer'
-                                sock.sendto(AlpacaDiscovery.encode(),
-                                            (ip['peer'], port))
-                            #
-                            # I know this is inefficient, but this way we can filter
-                            # out any of our local addresses (adapters). Plus we need to
-                            # keep trying to receive responses from possible multiple
-                            # servers out there till there are no more.
-                            #
-                            while True:                     # Keep receiving from devices until timeout
-                                try:
-                                    time.sleep(timeout / 5)     # Give at least a bit of time to respond
-                                    pinfo, rem = sock.recvfrom(1024)  # buffer size is 1024 bytes
-                                    remport = json.loads(pinfo.decode())["AlpacaPort"]
-                                    remip, p = rem
-                                    if(remip == addr and remip != '127.0.0.1'):
-                                        continue                # avoid router loop back to ourselves
-                                    ipp = f"{remip}:{remport}"
-                                    if ipp not in addrs:        # Avoid dupes if numquery > 1
-                                        addrs.append(ipp)
-                                except:
-                                    break                       # No more responses from the broadcast
+            for adapter in ifaddr.get_adapters():
+                for ip in adapter.ips:
+                    if(ip.is_IPv4):
+                        octets = ip.ip.split('.')
+                        if(len(octets) != 4):               # Quality check
+                            continue
+                        if octets[0] == '169' and octets[1] == '254':
+                            continue                        # APIPA address, skip
+                        # print(ip.ip)
+                        net = ipaddress.IPv4Network(f'{ip.ip}/{ip.network_prefix}', strict = False)
+                        # print(net.broadcast_address)
+                        n = sock.sendto(AlpacaDiscovery.encode(),(str(net.broadcast_address), port))
+                        time.sleep(timeout / 2)            # Give the server(s) a bit of time to respond
+                    while True:                         # Loop through response(s) till times out
+                        try:
+                            pinfo, rem = sock.recvfrom(1024)  # buffer size is 1024 bytes
+                            remport = json.loads(pinfo.decode())["AlpacaPort"]
+                            remip, p = rem
+                            if(remip == ip.ip and remip != '127.0.0.1'):
+                                continue                # avoid router loop back to ourselves
+                            ipp = f"{remip}:{remport}"
+                            if ipp not in addrs:        # Avoid dupes if numquery > 1
+                                addrs.append(ipp)
+                        except:
+                            break                       # leave while True (to next for)
+
+
     finally:
         sock.close()
     return addrs
-
 
 def search_ipv6(numquery: int=2, timeout: int=2) -> List[str]:
     """Discover Alpaca device servers on the IPV6 LAN/VLAN
@@ -160,48 +160,65 @@ def search_ipv6(numquery: int=2, timeout: int=2) -> List[str]:
     my_plat = platform.system()
     addrs = []
     for i in range(numquery):
-        for interface in netifaces.interfaces():
-            for interfacedata in netifaces.ifaddresses(interface):  # Sift through interfaces
-                 if netifaces.AF_INET6 == interfacedata:            # Consider only those with IPv6
-                    for info in netifaces.ifaddresses(interface)[netifaces.AF_INET6]:
-                        addr = info['addr']
-                        # Can't bind socket to ::1 and successfully send.
-                        # So lookfor source == dest and substitute later.
-                        # Reject everything but real link-local, including
-                        # the ISATAP addresses.
-                        if (not addr.startswith('fe80') or
-                                addr.startswith('fe80::5efe') or
-                                addr.startswith('fe80::200:5efe')):
-                            continue
-                        scope = addr.split('%')[1]
-                        try:
-                            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-                            if my_plat == 'Linux':
-                                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE,
-                                                (interface + '\0').encode())
-                            elif my_plat == "Windows":
-                                sock.bind((addr, 0))                    # Force send from this IP
-                            else:
-                                raise NotImplementedError('MacOS IPv6 discovery not yet supported')
-                            sock.settimeout(timeout)
-                            dest = 'ff12::a1:9aca'
-                            sock.sendto(AlpacaDiscovery.encode(), (dest, port))
-                            while True:
-                                try:
-                                    pinfo, rem = sock.recvfrom(1024)    # buffer size is 1024 bytes
-                                    remport = json.loads(pinfo.decode())["AlpacaPort"]
-                                    remip = rem[0]
-                                    if(addr.startswith(remip)):
-                                        ipp = f"[::1]:{remport}"        # Substitute loopback
-                                    else:
-                                        ipp = f"[{remip}%{scope}]:{remport}"    # External Alpaca
-                                    if ipp not in addrs:                # Avoid dupes if numquery > 1
-                                        addrs.append(ipp)
-                                except:
-                                    break
-                        finally:
-                            sock.close()
-
-    return addrs
-
-
+        for adapter in ifaddr.get_adapters():
+            addr = ''
+            for ip in adapter.ips:
+                if(ip.is_IPv6):
+                    addr = ip.ip[0]
+                    scope = ip.ip[2]
+                    # Reject everything but real link-local, including
+                    # the ISATAP addresses.
+                    if (not addr.startswith('fe80') or
+                            addr.startswith('fe80::5efe') or
+                            addr.startswith('fe80::200:5efe')):
+                        addr = ''
+                        continue
+                # This slime checks the IPV4 address of this adapter, and if it
+                # is an APIPA address (169.254.*.*), we skip this adapter as we
+                # did above. I don't know of another way to determine whether
+                # an adapter is alive with a real local address like using APIPA.
+                elif(ip.is_IPv4):
+                    octets = ip.ip.split('.')
+                    if(len(octets) != 4):               # Quality check
+                        addr = ''
+                    if octets[0] == '169' and octets[1] == '254':
+                        addr = ''
+            if(addr == ''):
+                continue
+            # print(f'local adapter on [{addr}]')
+            try:
+                sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+                if my_plat == 'Linux':
+                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE,
+                                    (adapter.name + '\0').encode())
+                elif my_plat == "Windows":
+                    sock.bind((addr, 0))                    # Force send from this IP
+                else:
+                    raise NotImplementedError('MacOS IPv6 discovery not yet supported')
+                sock.settimeout(timeout)
+                if(addr == '::1'):
+                    dest = addr
+                else:
+                    dest = 'ff12::a1:9aca'
+                sock.sendto(AlpacaDiscovery.encode(), (dest, port))
+                time.sleep(timeout / 2)                     # Give at least a bit of time to respond
+                while True:
+                    try:                                    # Loop through response(s) till times out
+                        pinfo, rem = sock.recvfrom(1024)    # buffer size is 1024 bytes
+                        remport = json.loads(pinfo.decode())["AlpacaPort"]
+                        remip = rem[0]
+                        # print(f'remote IP is {remip}')
+                        if(addr.startswith(remip)):
+                            # print(' This is us, loopback.')
+                            ipp = f"[::1]:{remport}"        # Substitute loopback
+                        else:
+                            ipp = f"[{remip}%{scope}]:{remport}"    # External Alpaca
+                        # print(f'Rcvd from {ipp}')
+                        if ipp not in addrs:                # Avoid dupes if numquery > 1
+                            # print(f'Adding {ipp}')
+                            addrs.append(ipp)
+                    except:
+                        break
+            finally:
+                sock.close()
+    return(addrs)
